f0a7bdc27dcbbc47045f697b196273efffd97e1b
JEISON101 <reyesjeison2006@fmail.com>
2025-10-29 16:52:30 -0500

    prueba exitosa

--- src/tests/Message.test.tsx
+++ src/tests/Message.test.tsx
@@ -4,5 +4,6 @@ import Message from "../components/Message"
 
 test('muestra de mensaje en pantalla', () => {
   render(<Message/>);
-  expect(screen.getByRole('p')).toHaveTextContent('hello')
+  //expect(screen.getByRole('p')).toHaveTextContent('hello')
+  expect(screen.getByText('hello')).toBeInTheDocument();
 })
